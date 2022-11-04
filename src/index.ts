594d50be660701665643f075c947b1d9486d5cbd
Max Blancarte <blancarte@gmail.com>
2022-11-04 14:31:32 -0700

    feat: add oak and endopints

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,48 @@
-import { serve } from "https://deno.land/std@0.85.0/http/server.ts";
+import { Application, send } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import * as log from "https://deno.land/std/log/mod.ts";
+import api from "./api.ts";
 
-let port = parseInt(Deno.env.get("PORT") ?? "8000")
-const s = serve({ port });
+const app = new Application();
+const PORT = parseInt(Deno.env.get("PORT") ?? "8000");
 
-console.log(`http://localhost:${port}/`);
+log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("INFO"),
+  },
+  loggers: {
+    default: {
+      level: "INFO",
+      handlers: ["console"],
+    },
+  },
+});
 
-for await (const req of s) {
-  req.respond({ body: "Choo Choo! Welcome to your Deno app\n" });
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (e) {
+    log.error(e);
+    ctx.response.body = "Internal Server Error";
+    throw e;
+  }
+});
+
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  log.info(`${ctx.request.method} ${ctx.request.url} ${time}`);
+});
+
+app.addEventListener("error", (e) => {
+  log.error(e.error);
+});
+
+app.use(api.routes());
+api.use(api.allowedMethods());
+
+if (import.meta.main) {
+  log.info(`Starting server on port ${PORT}`);
+  await app.listen({
+    port: PORT,
+  });
 }
